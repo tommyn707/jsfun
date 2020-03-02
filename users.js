905897861ee2a1a5407cd5bd212214e192ed8edb
tommyn707 <tommy.n707@gmail.com>
2020-03-01 20:02:27 -0800

    users

---
+++ users.js
@@ -0,0 +1,38 @@
+users = [
+    {
+      fname: "Kermit",
+      lname: "the Frog",
+      languages: ["Python", "JavaScript", "C#", "HTML", "CSS", "SQL"],
+      interests: {
+        music: ["guitar", "flute"],
+        dance: ["tap", "salsa"],
+        television: ["Black Mirror", "Stranger Things"]
+      }
+    },
+    {
+      fname: "Winnie",
+      lname: "the Pooh",
+      languages: ["Python", "Swift", "Java"],
+      interests: {
+        food: ["honey", "honeycomb"],
+        flowers: ["honeysuckle"],
+        mysteries: ["Heffalumps"]
+      }
+    },
+    {
+      fname: "Arthur",
+      lname: "Dent",
+      languages: ["JavaScript", "HTML", "Go"],
+      interests: {
+        space: ["stars", "planets", "improbability"],
+        home: ["tea", "yellow bulldozers"]
+      }
+    }
+  ]
+
+function userLanguages(arr){
+    for(i = 0; i < arr.length; i++){
+        console.log(arr[i].fname + " " + arr[i].lname + " knows " + arr[i].languages + ".");
+        return(arr[i].fname + " " + arr[i].lname + " knows " + arr[i].languages + ".")
+    }
+}
